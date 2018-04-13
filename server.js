5ee26ae15b982bcbcbb9307baa09ad76ddfdac86
Max Faneuff <maxfaneuff@gmail.com>
2018-04-12 21:34:03 -0400

    stuff2

--- server.js
+++ server.js
@@ -90,6 +90,6 @@ app.post("/api/characters", function (req, res) {
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function () {
-  console.log("App listening on PORT " + PORT);
+app.listen(port, function () {
+  console.log("App listening on PORT " + port);
 });
